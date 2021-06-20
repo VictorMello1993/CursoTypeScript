56940b7c407141f8ed1fa64195ab1a6dcc2d96db
usertest <emaildofulano@gmail.com>
2021-06-20 11:54:22 -0300

    Capítulo de interfaces

--- interfaces/interfaces.js
+++ interfaces/interfaces.js
@@ -39,4 +39,72 @@ myClient.name = 'Han';
 sayHello(myClient);
 myClient.greet('Solo');
 console.log(myClient.lastOrder);
+let power; //Variável 'power' implementa a interface Calculus, que no caso representa uma função
+power = function (base, exp) {
+    return Math.pow(base, exp);
+};
+console.log(power(2, 4));
+console.log(power(10, 2));
+console.log(power(25, 2));
+class classA {
+    methodA() { console.log('Método A da classe A'); }
+}
+class classB {
+    methodA() { console.log('Método B da classe B'); }
+    methodB() { console.log('Método A da classe B'); }
+}
+class classABC {
+    methodA() { console.log('Método A da classe ABC'); }
+    methodB() { console.log('Método B da classe ABC'); }
+    methodC() { console.log('Método C da classe ABC'); }
+}
+function newFunction(b) {
+    console.log('Este é um objeto do tipo interfaceB' + b);
+}
+newFunction(new classABC());
+let objA = new classA();
+let objB = new classB();
+let objABC = new classABC();
+objA.methodA();
+objB.methodB();
+objABC.methodA();
+objABC.methodB();
+objABC.methodC();
+/*OBS: Em TypeScript, uma vez declarada uma classe abstrata que implementa interfaces, é obrigatório implementá-las,
+ao contrário de outras linguagens como Java, que é opcional.*/
+class AbstractABC {
+    methodA() {
+        console.log('Método A da classe abstrata ABC');
+    }
+    methodB() {
+        console.log('Método B da classe abstrata ABC');
+    }
+}
+class ConcreteABC extends AbstractABC {
+    methodD() {
+        console.log('Este é o método abstrato implementado da classe abstrata AbstractABC');
+    }
+}
+let concABC = new ConcreteABC();
+concABC.methodA();
+concABC.methodB();
+concABC.methodD();
+//Usando interface para estender a classe Object
+//Ex:
+Object.prototype.log = function () {
+    console.log(this.toString());
+};
+const x = 1;
+const y = 2;
+const z = 3;
+x.log();
+y.log();
+z.log();
+const cli = {
+    name: 'Victor',
+    toString() {
+        return this.name;
+    }
+};
+cli.log();
 //# sourceMappingURL=interfaces.js.map
--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -56,3 +56,118 @@ myClient.name = 'Han'
 sayHello(myClient)
 myClient.greet('Solo')
 console.log(myClient.lastOrder)
+
+//Interface x tipo função
+interface Calculus {
+  (num1: number, num2: number) : number
+}
+
+let power: Calculus //Variável 'power' implementa a interface Calculus, que no caso representa uma função
+
+power = function(base: number, exp: number): number{
+  return base ** exp
+}
+
+console.log(power(2, 4))
+console.log(power(10, 2))
+console.log(power(25, 2))
+
+//Herança com interfaces
+interface interfaceA {
+  methodA(): void
+}
+
+interface interfaceB {
+  methodB(): void
+}
+
+interface ABC extends interfaceA, interfaceB {
+  methodC(): void
+}
+
+class classA implements interfaceA {
+  methodA(): void {console.log('Método A da classe A')}
+}
+
+class classB implements interfaceA, interfaceB  {
+  methodA(): void {console.log('Método B da classe B')}
+  methodB(): void {console.log('Método A da classe B')}
+}
+
+class classABC implements ABC{
+  methodA(): void {console.log('Método A da classe ABC')}
+  methodB(): void {console.log('Método B da classe ABC')}
+  methodC(): void {console.log('Método C da classe ABC')}
+}
+
+function newFunction(b: interfaceB) {
+  console.log('Este é um objeto do tipo interfaceB' + b)
+}
+
+newFunction(new classABC())
+
+let objA = new classA()
+let objB = new classB()
+let objABC = new classABC()
+
+objA.methodA()
+objB.methodB()
+objABC.methodA()
+objABC.methodB()
+objABC.methodC()
+
+/*OBS: Em TypeScript, uma vez declarada uma classe abstrata que implementa interfaces, é obrigatório implementá-las,
+ao contrário de outras linguagens como Java, que é opcional.*/
+abstract class AbstractABC implements interfaceA, interfaceB{
+  methodA(): void {
+    console.log('Método A da classe abstrata ABC')
+  }
+
+  methodB(): void {
+    console.log('Método B da classe abstrata ABC')
+  }
+
+  abstract methodD(): void
+}
+
+class ConcreteABC extends AbstractABC{
+  methodD(): void {
+    console.log('Este é o método abstrato implementado da classe abstrata AbstractABC')
+  }
+}
+
+let concABC = new ConcreteABC();
+concABC.methodA()
+concABC.methodB()
+concABC.methodD()
+
+//Usando interface para estender a classe Object
+//Ex:
+Object.prototype.log = function(){
+  console.log(this.toString())
+}
+
+const x = 1
+const y = 2
+const z = 3
+
+x.log()
+y.log()
+z.log()
+
+/*No exemplo acima, a função log() só funciona no contexto do Javascript. No contexto de Typescript, essa função não
+está disponível mesmo que conheça a classe Object e a herança de protótipo como ocorre no Javascript. Para resolver isso,
+utiliza-se uma interface para adicionar essa função.*/
+interface Object{
+  log(): void
+}
+
+const cli = {
+  name: 'Victor', 
+  toString(){
+    return this.name
+  }
+}
+
+cli.log()
+
